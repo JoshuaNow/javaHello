dd67d8b0ca699d65afe3bb35d815dafc85250b50
Joshua Lopez <jrenelopez92@gmail.com>
2021-05-14 14:01:26 -0500

    first commit

---
+++ src/hello/runner.java
@@ -0,0 +1,45 @@
+// class has a scope and can have many methods like main
+package hello;
+
+public class runner {
+	
+	//method has a scope this is called the "main" method.
+	public static void main(String[] args) {
+		
+		// variables
+		// Strings 
+		// char, booleans , integers, short, long , float, double, byte
+		// numeric? - int, short, float, double, long, byte
+		// text? - char, boolean, !string!
+		
+		// primitive variables
+		
+		int myInt = 1;
+		// wrapper classes
+		
+		
+		int numberVar;
+		numberVar = 12;
+		
+		float myfloat = 12.5f;
+	
+		String myString = "Cat";
+		System.out.println(myString);
+		myString = myString + " does this work";
+	
+		boolean flag = true;
+		
+		System.out.println("Hello World"); 
+		System.out.println(numberVar);
+		System.out.println(numberVar);
+		System.out.println(myString);
+		System.out.println(myInt);
+		System.out.println(myfloat);
+		
+		System.out.println(flag);
+		
+		
+
+	}
+
+}
